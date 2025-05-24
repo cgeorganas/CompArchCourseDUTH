69f31e04f33d3ad7b1f3fe21bd0f8cd4a1b454a6
cgeorganas <126613109+cgeorganas@users.noreply.github.com>
2025-05-24 23:29:28 +0300

    Added verification benchmark for all I_JAL type instructions.

---
+++ benchmarks/I_JAL_TYPE.s
@@ -0,0 +1,16 @@
+# This verification benchmark will set "a0" to a
+# non zero value if the result does not match the expected value.
+
+auipc	a1, 0
+jalr	a2, a1, 12
+li		a0, 1
+addi	a1, a1, 8
+xor		a0, a1, a2
+
+auipc	a1, 0
+jalr	a2, a1, 20
+li		a0, 1
+li		a0, 1
+li		a0, 1
+addi	a1, a1, 8
+xor		a0, a1, a2
--- rtl/simulation/tests.s
+++ rtl/simulation/tests.s
@@ -516,6 +516,22 @@ xor		a0, a3, a4
 # This verification benchmark will set "a0" to a
 # non zero value if the result does not match the expected value.
 
+auipc	a1, 0
+jalr	a2, a1, 12
+li		a0, 1
+addi	a1, a1, 8
+xor		a0, a1, a2
+
+auipc	a1, 0
+jalr	a2, a1, 20
+li		a0, 1
+li		a0, 1
+li		a0, 1
+addi	a1, a1, 8
+xor		a0, a1, a2
+# This verification benchmark will set "a0" to a
+# non zero value if the result does not match the expected value.
+
 
 
 # "LW" benchmark
@@ -1582,3 +1598,41 @@ lw		a3, 0(a1)
 lui		a4, 0x44332
 addi	a4, a4, 0x211
 xor		a0, a3, a4
+# This verification benchmark will set "a0" to a
+# non zero value if the result does not match the expected value.
+
+
+# "AUIPC" benchmark
+
+auipc	a1, 0
+auipc	a2, 0
+addi	a1, a1, 4
+xor		a0, a1, a2
+
+auipc	a1, 0
+nop
+auipc	a2, 0
+addi	a1, a1, 8
+xor		a0, a1, a2
+
+auipc	a1, 0x1
+auipc	a2, 0
+addi	a1, a1, 4
+lui		a3, 0x1
+add		a2, a2, a3
+xor		a0, a1, a2
+# This verification benchmark will set "a0" to a
+# non zero value if the result does not match the expected value.
+
+
+# "LUI" benchmark
+
+li		a1, 0xaaa
+lui		a1, 0x12345
+
+addi	a2, x0, 0x123
+slli	a2, a2, 8
+addi	a2, a2, 0x45
+slli	a2, a2, 12
+
+xor		a0, a1, a2
