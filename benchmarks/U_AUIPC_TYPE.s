76675486e30b6eb5d312342f26a01e962f3420f5
cgeorganas <126613109+cgeorganas@users.noreply.github.com>
2025-05-24 23:18:14 +0300

    Added verification benchmark for all U_AUIPC type instructions.

---
+++ benchmarks/U_AUIPC_TYPE.s
@@ -0,0 +1,23 @@
+# This verification benchmark will set "a0" to a
+# non zero value if the result does not match the expected value.
+
+
+# "AUIPC" benchmark
+
+auipc	a1, 0
+auipc	a2, 0
+addi	a1, a1, 4
+xor		a0, a1, a2
+
+auipc	a1, 0
+nop
+auipc	a2, 0
+addi	a1, a1, 8
+xor		a0, a1, a2
+
+auipc	a1, 0x1
+auipc	a2, 0
+addi	a1, a1, 4
+lui		a3, 0x1
+add		a2, a2, a3
+xor		a0, a1, a2
