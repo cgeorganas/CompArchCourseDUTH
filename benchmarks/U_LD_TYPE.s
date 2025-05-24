ef031cb3daeea829491d68fa0ca9e6ef1be7a52a
cgeorganas <126613109+cgeorganas@users.noreply.github.com>
2025-05-24 22:25:47 +0300

    Added verification benchmark for all U_LD type instructions.

---
+++ benchmarks/U_LD_TYPE.s
@@ -0,0 +1,15 @@
+# This verification benchmark will set "a0" to a
+# non zero value if the result does not match the expected value.
+
+
+# "LUI" benchmark
+
+li		a1, 0xaaa
+lui		a1, 0x12345
+
+addi	a2, x0, 0x123
+slli	a2, a2, 8
+addi	a2, a2, 0x45
+slli	a2, a2, 12
+
+xor		a0, a1, a2
