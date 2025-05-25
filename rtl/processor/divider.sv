abc7e6c3f8a64087c9c81e5ff761758a9f1e5555
cgeorganas <126613109+cgeorganas@users.noreply.github.com>
2025-05-25 22:32:16 +0300

    Massively improved divider.

--- rtl/processor/divider.sv
+++ rtl/processor/divider.sv
@@ -14,42 +14,76 @@ module divider(
 	output	logic			EX_alu_busy
 );
 
-logic input_changed, done;
-logic [31:0] num, denom;
 
-assign input_changed = (num!=opa)||(denom!=opb);
+logic signed_div, unsigned_div;
+assign signed_div = (ID_EX_alu_func==`ALU_DIV)||(ID_EX_alu_func==`ALU_REM);
+assign unsigned_div = (ID_EX_alu_func==`ALU_DIVU)||(ID_EX_alu_func==`ALU_REMU);
+
+logic [31:0] numerator, denominator;
+logic [31:0] ec_quotient, ec_remainder; // Edge cases
+logic edge_case;
 
 always_comb begin
-	case (ID_EX_alu_func)
-		`ALU_DIV, `ALU_REM:							done = (remainder<denom)&&(~input_changed);
-		default:									done = `TRUE;
-	endcase
+	if (opb==32'h0) begin
+		ec_quotient		= 32'hffff_ffff;
+		ec_remainder	= opa;
+		edge_case		= `TRUE;
+		numerator		= 32'h0;
+		denominator		= 32'h1;
+	end
+	else if ((signed_div)&&(opa==32'h8000_0000)) begin
+		ec_quotient		= opa;
+		ec_remainder	= 32'h0;
+		edge_case		= `TRUE;
+		numerator		= 32'h0;
+		denominator		= 32'h1;
+	end
+	else begin
+		ec_quotient		= 32'h0;
+		ec_remainder	= 32'h0;
+		edge_case		= `FALSE;
+		numerator		= (signed_div&&opa[31]) ? -opa : opa;
+		denominator		= (signed_div&&opb[31]) ? -opb : opb;
+	end
 end
-assign EX_alu_busy = ~done;
+
+logic [31:0] prev_numerator, prev_denominator;
+logic [31:0] nor_quotient, nor_remainder;
+
+logic input_changed;
+assign input_changed = (numerator!=prev_numerator)||(prev_denominator!=denominator);
+
+logic done;
+assign done = (nor_remainder<prev_denominator)&&(~input_changed);
 
 always_ff @(posedge clk) begin
 	if (rst) begin
-		num		<= 32'h0;
-		denom	<= 32'h0;
-		quotient	<= 32'h0;
-		remainder		<= 32'h0;
+		prev_numerator		<= 32'h0;
+		prev_denominator	<= 32'h0;
+		nor_quotient		<= 32'h0;
+		nor_remainder		<= 32'h1;
 	end
 	else begin
 		if (input_changed) begin
-			num		<= opa;
-			denom	<= opb;
-			quotient	<= 32'h0;
-			remainder		<= opa;
-		end
-		else if (opb==32'h0) begin
-			quotient	<= opa;
-			remainder		<= 32'hffff_ffff;
+			prev_numerator		<= numerator;
+			prev_denominator	<= denominator;
+			nor_quotient		<= 32'h0;
+			nor_remainder		<= numerator;
 		end
 		else if (~done) begin
-			quotient	<= quotient + 1;
-			remainder		<= remainder - denom;
+			nor_quotient		<= nor_quotient + 1;
+			nor_remainder		<= nor_remainder - prev_denominator;
 		end
 	end
 end
+
+logic sign_q, sign_r;
+assign sign_q = signed_div ? opa[31]^opb[31] : `FALSE;
+assign sign_r = signed_div ? opa[31] : `FALSE;
+
+assign quotient = edge_case ? ec_quotient : (sign_q ? -nor_quotient : nor_quotient);
+assign remainder = edge_case ? ec_remainder : (sign_r ? -nor_remainder : nor_remainder);
+
+assign EX_alu_busy = edge_case ? `FALSE : ~done;
 
 endmodule
--- rtl/simulation/tests.s
+++ rtl/simulation/tests.s
@@ -7,21 +7,50 @@ rem		a6, a1, a2
 xor		a0, a3, a5
 xor		a0, a4, a6
 
-li		a1, 12
-li		a2,	4
-li		a3, 3
-li		a4, 0
-div		a5, a1, a2
-rem		a6, a1, a2
-xor		a0, a3, a5
-xor		a0, a4, a6
-
-li		a1, 4
-li		a2,	6
+li		a1, 0
+li		a2, 0
 li		a3, 0
-li		a4, 4
+li		a4, 0
+li		a5, 0
+li		a6, 0
+
+li		a1, 20
+li		a2,	-3
+li		a3, -6
+li		a4, 2
 div		a5, a1, a2
 rem		a6, a1, a2
 xor		a0, a3, a5
 xor		a0, a4, a6
 
+li		a1, 0
+li		a2, 0
+li		a3, 0
+li		a4, 0
+li		a5, 0
+li		a6, 0
+
+li		a1, -20
+li		a2,	3
+li		a3, -6
+li		a4, -2
+div		a5, a1, a2
+rem		a6, a1, a2
+xor		a0, a3, a5
+xor		a0, a4, a6
+
+li		a1, 0
+li		a2, 0
+li		a3, 0
+li		a4, 0
+li		a5, 0
+li		a6, 0
+
+li		a1, -20
+li		a2,	-3
+li		a3, 6
+li		a4, -2
+div		a5, a1, a2
+rem		a6, a1, a2
+xor		a0, a3, a5
+xor		a0, a4, a6
