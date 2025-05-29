89054f6d7a881ed4334c53ca9a10d8f946acd53a
cgeorganas <126613109+cgeorganas@users.noreply.github.com>
2025-05-29 20:13:43 +0300

    Added very basic flt2int conversion logic.

--- rtl/processor/fpu_flt2int.sv
+++ rtl/processor/fpu_flt2int.sv
@@ -8,6 +8,16 @@ module fpu_flt2int(
 	output	logic	[34:0]	out
 );
 
+logic [7:0]		exponent;
+assign			exponent = in[30:23];
 
+logic [22:0]	mantissa;
+assign			mantissa = in[22:0];
+
+logic [57:0]	fixed_initial, fixed; // Fixed point intermediate res, 32 integer bits, 26 binary bits
+assign			fixed_initial = {35'h1, mantissa};
+assign			fixed = fixed_initial << (exponent-124);
+
+assign 			out = {fixed[57:24], |fixed[23:0]};
 
 endmodule
