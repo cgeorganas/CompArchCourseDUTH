a1c2114a917f2190c637e6e311e7b4f21202bb3f
cgeorganas <126613109+cgeorganas@users.noreply.github.com>
2025-05-29 11:48:00 +0300

    Completed the implementation of FCVT.S.W and FCVT.S.WU instructions.

---
+++ benchmarks/RV32F/R_FLT_TYPE/FCVT.S.W.s
@@ -0,0 +1,287 @@
+li			a1, 0
+
+fcvt.s.w	f1, a1, rne
+fmv.x.w		a2, f1
+li			a3, 0
+xor			a0, a2, a3
+
+fcvt.s.w	f1, a1, rtz
+fmv.x.w		a2, f1
+li			a3, 0
+xor			a0, a2, a3
+
+fcvt.s.w	f1, a1, rdn
+fmv.x.w		a2, f1
+li			a3, 0
+xor			a0, a2, a3
+
+fcvt.s.w	f1, a1, rup
+fmv.x.w		a2, f1
+li			a3, 0
+xor			a0, a2, a3
+
+fcvt.s.w	f1, a1, rmm
+fmv.x.w		a2, f1
+li			a3, 0
+xor			a0, a2, a3
+
+
+
+li			a1, 24
+
+fcvt.s.w	f1, a1, rne
+fmv.x.w		a2, f1
+li			a3, 0x41c00000
+xor			a0, a2, a3
+
+fcvt.s.w	f1, a1, rtz
+fmv.x.w		a2, f1
+li			a3, 0x41c00000
+xor			a0, a2, a3
+
+fcvt.s.w	f1, a1, rdn
+fmv.x.w		a2, f1
+li			a3, 0x41c00000
+xor			a0, a2, a3
+
+fcvt.s.w	f1, a1, rup
+fmv.x.w		a2, f1
+li			a3, 0x41c00000
+xor			a0, a2, a3
+
+fcvt.s.w	f1, a1, rmm
+fmv.x.w		a2, f1
+li			a3, 0x41c00000
+xor			a0, a2, a3
+
+
+
+li			a1, -24
+
+fcvt.s.w	f1, a1, rne
+fmv.x.w		a2, f1
+li			a3, 0xc1c00000
+xor			a0, a2, a3
+
+fcvt.s.w	f1, a1, rtz
+fmv.x.w		a2, f1
+li			a3, 0xc1c00000
+xor			a0, a2, a3
+
+fcvt.s.w	f1, a1, rdn
+fmv.x.w		a2, f1
+li			a3, 0xc1c00000
+xor			a0, a2, a3
+
+fcvt.s.w	f1, a1, rup
+fmv.x.w		a2, f1
+li			a3, 0xc1c00000
+xor			a0, a2, a3
+
+fcvt.s.w	f1, a1, rmm
+fmv.x.w		a2, f1
+li			a3, 0xc1c00000
+xor			a0, a2, a3
+
+
+
+li			a1, 0x01000001
+
+fcvt.s.w	f1, a1, rne
+fmv.x.w		a2, f1
+li			a3, 0x4b800000
+xor			a0, a2, a3
+
+fcvt.s.w	f1, a1, rtz
+fmv.x.w		a2, f1
+li			a3, 0x4b800000
+xor			a0, a2, a3
+
+fcvt.s.w	f1, a1, rdn
+fmv.x.w		a2, f1
+li			a3, 0x4b800000
+xor			a0, a2, a3
+
+fcvt.s.w	f1, a1, rup
+fmv.x.w		a2, f1
+li			a3, 0x4b800001
+xor			a0, a2, a3
+
+fcvt.s.w	f1, a1, rmm
+fmv.x.w		a2, f1
+li			a3, 0x4b800001
+xor			a0, a2, a3
+
+
+
+li			a1, 0xfeffffff
+
+fcvt.s.w	f1, a1, rne
+fmv.x.w		a2, f1
+li			a3, 0xcb800000
+xor			a0, a2, a3
+
+fcvt.s.w	f1, a1, rtz
+fmv.x.w		a2, f1
+li			a3, 0xcb800000
+xor			a0, a2, a3
+
+fcvt.s.w	f1, a1, rdn
+fmv.x.w		a2, f1
+li			a3, 0xcb800001
+xor			a0, a2, a3
+
+fcvt.s.w	f1, a1, rup
+fmv.x.w		a2, f1
+li			a3, 0xcb800000
+xor			a0, a2, a3
+
+fcvt.s.w	f1, a1, rmm
+fmv.x.w		a2, f1
+li			a3, 0xcb800001
+xor			a0, a2, a3
+
+
+
+li			a1, 0x7fffffc0
+
+fcvt.s.w	f1, a1, rne
+fmv.x.w		a2, f1
+li			a3, 0x4f000000
+xor			a0, a2, a3
+
+fcvt.s.w	f1, a1, rtz
+fmv.x.w		a2, f1
+li			a3, 0x4effffff
+xor			a0, a2, a3
+
+fcvt.s.w	f1, a1, rdn
+fmv.x.w		a2, f1
+li			a3, 0x4effffff
+xor			a0, a2, a3
+
+fcvt.s.w	f1, a1, rup
+fmv.x.w		a2, f1
+li			a3, 0x4f000000
+xor			a0, a2, a3
+
+fcvt.s.w	f1, a1, rmm
+fmv.x.w		a2, f1
+li			a3, 0x4f000000
+xor			a0, a2, a3
+
+
+
+li			a1, 0x80000040
+
+fcvt.s.w	f1, a1, rne
+fmv.x.w		a2, f1
+li			a3, 0xcf000000
+xor			a0, a2, a3
+
+fcvt.s.w	f1, a1, rtz
+fmv.x.w		a2, f1
+li			a3, 0xceffffff
+xor			a0, a2, a3
+
+fcvt.s.w	f1, a1, rdn
+fmv.x.w		a2, f1
+li			a3, 0xcf000000
+xor			a0, a2, a3
+
+fcvt.s.w	f1, a1, rup
+fmv.x.w		a2, f1
+li			a3, 0xceffffff
+xor			a0, a2, a3
+
+fcvt.s.w	f1, a1, rmm
+fmv.x.w		a2, f1
+li			a3, 0xcf000000
+xor			a0, a2, a3
+
+
+
+li			a1, 0x7fffffff
+
+fcvt.s.w	f1, a1, rne
+fmv.x.w		a2, f1
+li			a3, 0x4f000000
+xor			a0, a2, a3
+
+fcvt.s.w	f1, a1, rtz
+fmv.x.w		a2, f1
+li			a3, 0x4effffff
+xor			a0, a2, a3
+
+fcvt.s.w	f1, a1, rdn
+fmv.x.w		a2, f1
+li			a3, 0x4effffff
+xor			a0, a2, a3
+
+fcvt.s.w	f1, a1, rup
+fmv.x.w		a2, f1
+li			a3, 0x4f000000
+xor			a0, a2, a3
+
+fcvt.s.w	f1, a1, rmm
+fmv.x.w		a2, f1
+li			a3, 0x4f000000
+xor			a0, a2, a3
+
+
+
+li			a1, 0x80000001
+
+fcvt.s.w	f1, a1, rne
+fmv.x.w		a2, f1
+li			a3, 0xcf000000
+xor			a0, a2, a3
+
+fcvt.s.w	f1, a1, rtz
+fmv.x.w		a2, f1
+li			a3, 0xceffffff
+xor			a0, a2, a3
+
+fcvt.s.w	f1, a1, rdn
+fmv.x.w		a2, f1
+li			a3, 0xcf000000
+xor			a0, a2, a3
+
+fcvt.s.w	f1, a1, rup
+fmv.x.w		a2, f1
+li			a3, 0xceffffff
+xor			a0, a2, a3
+
+fcvt.s.w	f1, a1, rmm
+fmv.x.w		a2, f1
+li			a3, 0xcf000000
+xor			a0, a2, a3
+
+
+
+li			a1, 0x80000000
+
+fcvt.s.w	f1, a1, rne
+fmv.x.w		a2, f1
+li			a3, 0xcf000000
+xor			a0, a2, a3
+
+fcvt.s.w	f1, a1, rtz
+fmv.x.w		a2, f1
+li			a3, 0xcf000000
+xor			a0, a2, a3
+
+fcvt.s.w	f1, a1, rdn
+fmv.x.w		a2, f1
+li			a3, 0xcf000000
+xor			a0, a2, a3
+
+fcvt.s.w	f1, a1, rup
+fmv.x.w		a2, f1
+li			a3, 0xcf000000
+xor			a0, a2, a3
+
+fcvt.s.w	f1, a1, rmm
+fmv.x.w		a2, f1
+li			a3, 0xcf000000
+xor			a0, a2, a3
---
+++ benchmarks/RV32F/R_FLT_TYPE/FCVT.S.WU.s
@@ -0,0 +1,287 @@
+li			a1, 0
+
+fcvt.s.wu	f1, a1, rne
+fmv.x.w		a2, f1
+li			a3, 0
+xor			a0, a2, a3
+
+fcvt.s.wu	f1, a1, rtz
+fmv.x.w		a2, f1
+li			a3, 0
+xor			a0, a2, a3
+
+fcvt.s.wu	f1, a1, rdn
+fmv.x.w		a2, f1
+li			a3, 0
+xor			a0, a2, a3
+
+fcvt.s.wu	f1, a1, rup
+fmv.x.w		a2, f1
+li			a3, 0
+xor			a0, a2, a3
+
+fcvt.s.wu	f1, a1, rmm
+fmv.x.w		a2, f1
+li			a3, 0
+xor			a0, a2, a3
+
+
+
+li			a1, 24
+
+fcvt.s.wu	f1, a1, rne
+fmv.x.w		a2, f1
+li			a3, 0x41c00000
+xor			a0, a2, a3
+
+fcvt.s.wu	f1, a1, rtz
+fmv.x.w		a2, f1
+li			a3, 0x41c00000
+xor			a0, a2, a3
+
+fcvt.s.wu	f1, a1, rdn
+fmv.x.w		a2, f1
+li			a3, 0x41c00000
+xor			a0, a2, a3
+
+fcvt.s.wu	f1, a1, rup
+fmv.x.w		a2, f1
+li			a3, 0x41c00000
+xor			a0, a2, a3
+
+fcvt.s.wu	f1, a1, rmm
+fmv.x.w		a2, f1
+li			a3, 0x41c00000
+xor			a0, a2, a3
+
+
+
+li			a1, -24
+
+fcvt.s.wu	f1, a1, rne
+fmv.x.w		a2, f1
+li			a3, 0x4f800000
+xor			a0, a2, a3
+
+fcvt.s.wu	f1, a1, rtz
+fmv.x.w		a2, f1
+li			a3, 0x4f7fffff
+xor			a0, a2, a3
+
+fcvt.s.wu	f1, a1, rdn
+fmv.x.w		a2, f1
+li			a3, 0x4f7fffff
+xor			a0, a2, a3
+
+fcvt.s.wu	f1, a1, rup
+fmv.x.w		a2, f1
+li			a3, 0x4f800000
+xor			a0, a2, a3
+
+fcvt.s.wu	f1, a1, rmm
+fmv.x.w		a2, f1
+li			a3, 0x4f800000
+xor			a0, a2, a3
+
+
+
+li			a1, 0x01000001
+
+fcvt.s.wu	f1, a1, rne
+fmv.x.w		a2, f1
+li			a3, 0x4b800000
+xor			a0, a2, a3
+
+fcvt.s.wu	f1, a1, rtz
+fmv.x.w		a2, f1
+li			a3, 0x4b800000
+xor			a0, a2, a3
+
+fcvt.s.wu	f1, a1, rdn
+fmv.x.w		a2, f1
+li			a3, 0x4b800000
+xor			a0, a2, a3
+
+fcvt.s.wu	f1, a1, rup
+fmv.x.w		a2, f1
+li			a3, 0x4b800001
+xor			a0, a2, a3
+
+fcvt.s.wu	f1, a1, rmm
+fmv.x.w		a2, f1
+li			a3, 0x4b800001
+xor			a0, a2, a3
+
+
+
+li			a1, 0xfeffffff
+
+fcvt.s.wu	f1, a1, rne
+fmv.x.w		a2, f1
+li			a3, 0x4f7f0000
+xor			a0, a2, a3
+
+fcvt.s.wu	f1, a1, rtz
+fmv.x.w		a2, f1
+li			a3, 0x4f7effff
+xor			a0, a2, a3
+
+fcvt.s.wu	f1, a1, rdn
+fmv.x.w		a2, f1
+li			a3, 0x4f7effff
+xor			a0, a2, a3
+
+fcvt.s.wu	f1, a1, rup
+fmv.x.w		a2, f1
+li			a3, 0x4f7f0000
+xor			a0, a2, a3
+
+fcvt.s.wu	f1, a1, rmm
+fmv.x.w		a2, f1
+li			a3, 0x4f7f0000
+xor			a0, a2, a3
+
+
+
+li			a1, 0x7fffffc0
+
+fcvt.s.wu	f1, a1, rne
+fmv.x.w		a2, f1
+li			a3, 0x4f000000
+xor			a0, a2, a3
+
+fcvt.s.wu	f1, a1, rtz
+fmv.x.w		a2, f1
+li			a3, 0x4effffff
+xor			a0, a2, a3
+
+fcvt.s.wu	f1, a1, rdn
+fmv.x.w		a2, f1
+li			a3, 0x4effffff
+xor			a0, a2, a3
+
+fcvt.s.wu	f1, a1, rup
+fmv.x.w		a2, f1
+li			a3, 0x4f000000
+xor			a0, a2, a3
+
+fcvt.s.wu	f1, a1, rmm
+fmv.x.w		a2, f1
+li			a3, 0x4f000000
+xor			a0, a2, a3
+
+
+
+li			a1, 0x80000040
+
+fcvt.s.wu	f1, a1, rne
+fmv.x.w		a2, f1
+li			a3, 0x4f000000
+xor			a0, a2, a3
+
+fcvt.s.wu	f1, a1, rtz
+fmv.x.w		a2, f1
+li			a3, 0x4f000000
+xor			a0, a2, a3
+
+fcvt.s.wu	f1, a1, rdn
+fmv.x.w		a2, f1
+li			a3, 0x4f000000
+xor			a0, a2, a3
+
+fcvt.s.wu	f1, a1, rup
+fmv.x.w		a2, f1
+li			a3, 0x4f000001
+xor			a0, a2, a3
+
+fcvt.s.wu	f1, a1, rmm
+fmv.x.w		a2, f1
+li			a3, 0x4f000000
+xor			a0, a2, a3
+
+
+
+li			a1, 0x7fffffff
+
+fcvt.s.wu	f1, a1, rne
+fmv.x.w		a2, f1
+li			a3, 0x4f000000
+xor			a0, a2, a3
+
+fcvt.s.wu	f1, a1, rtz
+fmv.x.w		a2, f1
+li			a3, 0x4effffff
+xor			a0, a2, a3
+
+fcvt.s.wu	f1, a1, rdn
+fmv.x.w		a2, f1
+li			a3, 0x4effffff
+xor			a0, a2, a3
+
+fcvt.s.wu	f1, a1, rup
+fmv.x.w		a2, f1
+li			a3, 0x4f000000
+xor			a0, a2, a3
+
+fcvt.s.wu	f1, a1, rmm
+fmv.x.w		a2, f1
+li			a3, 0x4f000000
+xor			a0, a2, a3
+
+
+
+li			a1, 0x80000001
+
+fcvt.s.wu	f1, a1, rne
+fmv.x.w		a2, f1
+li			a3, 0x4f000000
+xor			a0, a2, a3
+
+fcvt.s.wu	f1, a1, rtz
+fmv.x.w		a2, f1
+li			a3, 0x4f000000
+xor			a0, a2, a3
+
+fcvt.s.wu	f1, a1, rdn
+fmv.x.w		a2, f1
+li			a3, 0x4f000000
+xor			a0, a2, a3
+
+fcvt.s.wu	f1, a1, rup
+fmv.x.w		a2, f1
+li			a3, 0x4f000001
+xor			a0, a2, a3
+
+fcvt.s.wu	f1, a1, rmm
+fmv.x.w		a2, f1
+li			a3, 0x4f000000
+xor			a0, a2, a3
+
+
+
+li			a1, 0x80000000
+
+fcvt.s.wu	f1, a1, rne
+fmv.x.w		a2, f1
+li			a3, 0x4f000000
+xor			a0, a2, a3
+
+fcvt.s.wu	f1, a1, rtz
+fmv.x.w		a2, f1
+li			a3, 0x4f000000
+xor			a0, a2, a3
+
+fcvt.s.wu	f1, a1, rdn
+fmv.x.w		a2, f1
+li			a3, 0x4f000000
+xor			a0, a2, a3
+
+fcvt.s.wu	f1, a1, rup
+fmv.x.w		a2, f1
+li			a3, 0x4f000000
+xor			a0, a2, a3
+
+fcvt.s.wu	f1, a1, rmm
+fmv.x.w		a2, f1
+li			a3, 0x4f000000
+xor			a0, a2, a3
